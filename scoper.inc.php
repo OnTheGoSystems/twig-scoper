90c5728fe726598027a278324919da88ba8052e6
jan <jan.s@onthegosystems.com>
2019-07-10 17:22:24 +0200

    Configure php-scoper to produce a working result (so it seems). Obtain the source Twig library via composer.

--- scoper.inc.php
+++ scoper.inc.php
@@ -2,70 +2,121 @@
 
 declare(strict_types=1);
 
-use Isolated\Symfony\Component\Finder\Finder;
+namespace OTGS\Toolset\TwigPrefixer {
 
-return [
-    // The prefix configuration. If a non null value will be used, a random prefix will be generated.
-    'prefix' => 'OTGS',
+	const TWIG_BASE_DIR = __DIR__ . '/vendor/twig/twig';
 
-    // By default when running php-scoper add-prefix, it will prefix all relevant code found in the current working
-    // directory. You can however define which files should be scoped by defining a collection of Finders in the
-    // following configuration key.
-    //
-    // For more see: https://github.com/humbug/php-scoper#finders-and-paths
-    'finders' => [
-        Finder::create()->files()->in('src/lib'),
-        Finder::create()->files()->in('src/src'),
-    ],
+	function endsWith( $haystack, $needle ) {
+		$length = strlen( $needle );
+		if ( $length == 0 ) {
+			return true;
+		}
 
-    // Whitelists a list of files. Unlike the other whitelist related features, this one is about completely leaving
-    // a file untouched.
-    // Paths are relative to the configuration file unless if they are already absolute
-    'files-whitelist' => [
-        'src/a-whitelisted-file.php',
-    ],
+		return ( substr( $haystack, - $length ) === $needle );
+	}
 
-    // When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
-    // original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
-    // support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
-    // heart contents.
-    //
-    // For more see: https://github.com/humbug/php-scoper#patchers
-    'patchers' => [
-        function (string $filePath, string $prefix, string $contents): string {
-            // Change the contents here.
+}
 
-            return $contents;
-        },
-    ],
+namespace {
 
-    // PHP-Scoper's goal is to make sure that all code for a project lies in a distinct PHP namespace. However, you
-    // may want to share a common API between the bundled code of your PHAR and the consumer code. For example if
-    // you have a PHPUnit PHAR with isolated code, you still want the PHAR to be able to understand the
-    // PHPUnit\Framework\TestCase class.
-    //
-    // A way to achieve this is by specifying a list of classes to not prefix with the following configuration key. Note
-    // that this does not work with functions or constants neither with classes belonging to the global namespace.
-    //
-    // Fore more see https://github.com/humbug/php-scoper#whitelist
-    'whitelist' => [
-        // 'PHPUnit\Framework\TestCase',   // A specific class
-        // 'PHPUnit\Framework\*',          // The whole namespace
-        // '*',                            // Everything
-    ],
+	use Isolated\Symfony\Component\Finder\Finder;
+	use const OTGS\Toolset\TwigPrefixer\TWIG_BASE_DIR;
 
-    // If `true` then the user defined constants belonging to the global namespace will not be prefixed.
-    //
-    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-constants' => true,
+	return [
+		// The prefix configuration. If a non null value will be used, a random prefix will be generated.
+		'prefix' => 'OTGS\Toolset',
 
-    // If `true` then the user defined classes belonging to the global namespace will not be prefixed.
-    //
-    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-classes' => true,
+		// For more see: https://github.com/humbug/php-scoper#finders-and-paths
+		'finders' => [
+			Finder::create()->files()->in( TWIG_BASE_DIR . '/lib' ),
+			Finder::create()->files()->in( TWIG_BASE_DIR . '/src' ),
+		],
 
-    // If `true` then the user defined functions belonging to the global namespace will not be prefixed.
-    //
-    // For more see https://github.com/humbug/php-scoper#constants--constants--functions-from-the-global-namespace
-    'whitelist-global-functions' => true,
-];
+		// When scoping PHP files, there will be scenarios where some of the code being scoped indirectly references the
+		// original namespace. These will include, for example, strings or string manipulations. PHP-Scoper has limited
+		// support for prefixing such strings. To circumvent that, you can define patchers to manipulate the file to your
+		// heart contents.
+		//
+		// For more see: https://github.com/humbug/php-scoper#patchers
+		'patchers' => [
+
+			/**
+			 * Patcher for all files.
+			 */
+			function ( string $filePath, string $prefix, string $contents ): string {
+				// Hardcoded class names in code
+				$contents = preg_replace(
+					'/("|\')((\\\\){1,2}Twig(\\\\){1,2}[A-Za-z\\\\]+)\1/m',
+					'$1\\\\\\\\OTGS\\\\\\\\Toolset$2$1',
+					$contents
+				);
+
+				// Hardcoded "use" statements
+				$contents = preg_replace(
+					'/use\s+(Twig)(\\\\){1,2}/m',
+					'use \\\\\\\\OTGS\\\\\\\\Toolset\\\\\\\\Twig\\\\\\\\',
+					$contents
+				);
+
+				// Add namespaces to generated Twig template names
+				$contents = preg_replace(
+					'/(\'|")(__TwigTemplate_)\1/m',
+					'$1\\\\\\\\OTGS\\\\\\\\Toolset\\\\\\\\$2$1',
+					$contents
+				);
+
+				return $contents;
+			},
+
+			/**
+			 * Patcher for \OTGS\Toolset\Twig\Node\ModuleNode.
+			 */
+			function ( string $filePath, string $prefix, string $contents ): string {
+				if ( \OTGS\Toolset\TwigPrefixer\endsWith( $filePath, 'src/Node/ModuleNode.php' ) ) {
+					// Fix template compilation - add the namespace to the template file.
+					$contents = preg_replace(
+						'/(compileClassHeader\s*\([^\)]+\)\s*{\s*\s*\$compiler\s*->\s*write\s*\(\s*)"\\\\n\\\\n"(\s*\)\s*;)/m',
+						'$1"\\n\\nnamespace OTGS\\\\Toolset;\\n\\n"$2',
+						$contents
+					);
+
+					// When generating the PHP template, make sure its class declaration doesn't contain the namespace.
+					// That's the only place where we don't want to have it.
+					$string_to_remove = '\\OTGS\\Toolset\\';
+					$contents = preg_replace(
+						'/(->write\s*\(\s*\'class \'\s*\.\s*)(\$compiler\s*->\s*getEnvironment\s*\(\s*\)\s*->\s*getTemplateClass\s*\(\s*\$this\s*->\s*getSourceContext\s*\(\s*\)\s*->\s*getName\s*\(\s*\)\s*,\s*\$this\s*->\s*getAttribute\s*\(\s*\'index\'\s*\)\s*\))/m',
+						'$1 \\substr( $2, ' . strlen( $string_to_remove ) . ' ) ',
+						$contents
+					);
+				}
+
+				return $contents;
+			},
+
+			/**
+			 * Patcher for \OTGS\Toolset\Twig\Extension\CoreExtension.
+			 */
+			function ( string $filePath, string $prefix, string $contents ): string {
+				// Fix the usage of global twig_* and _twig_* functions.
+				if ( \OTGS\Toolset\TwigPrefixer\endsWith( $filePath, 'src/Extension/CoreExtension.php' ) ) {
+					$contents = preg_replace(
+						'/(new \\\\OTGS\\\\Toolset\\\\Twig\\\\TwigFilter\(\s*\'[^\']+\'\s*,\s*\')((_)?twig_[^\']+\')/m',
+						'$1\\\\\\\\OTGS\\\\\\\\Toolset\\\\\\\\$2',
+						$contents
+					);
+
+					// Also handle the occurrence in the is_safe_callback array element.
+					$contents = preg_replace(
+						'/(new \\\\OTGS\\\\Toolset\\\\Twig\\\\TwigFilter\(\s*\'[^\']+\'\s*,\s*\'.*twig_[^\']+\',\s*\[[^\]]*,\s*\'is_safe_callback\'\s*=>\s*\')((_)?twig_[^\']+\'\s*\]\s*\))/m',
+						'$1\\\\\\\\OTGS\\\\\\\\Toolset\\\\\\\\$2',
+						$contents
+					);
+
+				}
+
+				return $contents;
+			},
+		],
+	];
+
+}
